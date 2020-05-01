ac6c81ef33866f8e40ea6b0af3ea186b0d739544
Yuchi <amayuchi0055@gmail.com>
2020-05-01 20:31:34 +0900

    メモ化によるフィボナッチ数列

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 'use strict';
+const memo = new Map();
+memo.set(0,0);
+memo.set(1,1);
+
+function fib(n){
+    if(memo.has(n)){
+        return memo.get(n);
+    }
+    const value = memo.get(n-1) + memo.get(n-2);
+    memo.set(n, value);
+    return memo.get(n);
+
+}
+
+const length = 40;
+for(let i=1; i<=length; i++){
+    console.log(fib(i));
+}
